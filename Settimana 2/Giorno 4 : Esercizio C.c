495e2a5ff9649a338745c65c3e075ad56460cdff
Gianx <50986754+gianrag@users.noreply.github.com>
2024-04-18 17:49:53 +0200

    Update Giorno 4 : Esercizio C.c

--- Settimana 2/Giorno 4 : Esercizio C.c
+++ Settimana 2/Giorno 4 : Esercizio C.c
@@ -5,10 +5,19 @@ int main()
     int points = 0;
     int number;
     char exitGame = 'Y';
+    char name [20];
+    char play;
     
     printf("--- Welcome to the quiz game ---\nTo win this game you need to make 3 points\n");
     printf("--------------------------------------------\nSo let's begin motherflower!\n");
     printf("You need to choose 1 , 2 or 3\n");
+    printf("Do you wanna play? (y / n)\n");
+    scanf("%s", &play);
+    
+    if(play == 'y'){
+    
+    printf("Insert your name\n");
+    scanf("%s", &name);
     
     do {
     points = 0;
@@ -35,11 +44,14 @@ int main()
     }
     
     //Results
-    printf("\nYour final score is %d/3 !! ", points);
-    printf("Do you want to play again?\n Y / N\n\n");
+    printf("\n%s your final score is %d/3 !! ", name, points);
+    printf("Do you want to play again?\n (y / n)\n\n");
     scanf("%s", &exitGame);
     
-    } while(exitGame == 'Y');
+    } while(exitGame == 'y');
 
+   } else {
+      printf("Alright c ya!");
+  }
     return 0;
 }
